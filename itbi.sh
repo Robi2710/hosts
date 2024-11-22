7fab2f07c8e0b0b04c1f398f91d0a1db13a3c6e4
Robi2710 <robertroby369@gmail.com>
2024-11-22 11:20:57 +0200

    .

--- itbi.sh
+++ itbi.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+validate_ip() {
+    local host=$1
+    local ip=$2
+    local dns_server=$3
+
+    lookup_ip=$(nslookup $host $dns_server | grep "Address" | tail -n 1 | awk '{print $2}')
+    
+    if [[ $ip != $lookup_ip ]]; then
+        echo "Bogus IP for $host in /etc/hosts! Expected: $ip, Found: $lookup_ip"
+    fi
+}
 cat /etc/hosts | while read -r line; do
     if [[ $line =~ ^# ]] || [[ -z $line ]]; then
         continue
     fi
-    ip=$(echo $line | awk '{print $1}')
+        ip=$(echo $line | awk '{print $1}')
     host=$(echo $line | awk '{print $2}')
-    lookup_ip=$(nslookup $host | grep "Address" | tail -n 1 | awk '{print $2}')
-    if [[ $ip != $lookup_ip ]]; then
-        echo "Bogus IP for $host in /etc/hosts!"
-    fi
+        validate_ip $host $ip 8.8.8.8
 done
